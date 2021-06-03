b13dd8c34a56425eae871fe6da50bf2eb17c74e7
Kevin <kevinalexanderduran8@gmail.com>
2021-06-03 10:56:38 -0700

    test

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
-var tag = document.createElement('script');
+gsap.to('.header--content__img--img',{
+    opacity:1, 
+    duration:1,
+    delay:0.2,
+     y:-50
+});
+gsap.to('.header--content__desc',{
+    opacity:1, 
+    delay:0.5,
+    duration:1,
+     y:-50
+});
 
-tag.src = "//www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var player;
+gsap.to('.num1',{
+    opacity:1, 
+    delay:0.2,
+    duration:1,
+    scrollTrigger: '.num1',
+     y:-50,
+     
+});
 
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('ytplayer', {
-        events: {
-            'onReady': onPlayerReady
-        }
-    });
-}
+gsap.to('.num2',{
+    opacity:1, 
+    delay:0.5,
+    duration:1,
+    scrollTrigger: '.num2',
+     y:-50
+});
 
-function onPlayerReady() {
-    player.playVideo();
-    // Mute!
-    player.mute();
-}
+gsap.to('.num3',{
+    opacity:1, 
+    delay:0.2,
+    duration:1,
+    scrollTrigger: '.num3',
+     y:-50
+});
+gsap.to('.num4',{
+    opacity:1, 
+    delay:0.5,
+    duration:1,
+    scrollTrigger: '.num4',
+     y:-50
+});
+gsap.to('.num5',{
+    opacity:1, 
+    delay:0.2,
+    duration:1,
+    scrollTrigger: '.num5',
+     y:-50
+});
+gsap.to('.num6',{
+    opacity:1, 
+    delay:0.5,
+    duration:1,
+    scrollTrigger: '.num6',
+     y:-50
+});

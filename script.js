2b8c50f9efe40e056fdd80db459663841f9c011f
Kevin <kevinalexanderduran8@gmail.com>
2021-06-03 21:36:46 -0700

    test

--- script.js
+++ script.js
@@ -15,45 +15,85 @@ gsap.to('.header--content__desc',{
 gsap.to('.num1',{
     opacity:1, 
     delay:0.2,
-    duration:1,
+    duration:0.5,
     scrollTrigger: '.num1',
-     y:-50,
-     
+     y:-210,
 });
 
 gsap.to('.num2',{
     opacity:1, 
     delay:0.5,
-    duration:1,
+    duration:0.5,
     scrollTrigger: '.num2',
-     y:-50
+     y:-210
 });
 
 gsap.to('.num3',{
     opacity:1, 
     delay:0.2,
-    duration:1,
+    duration:0.5,
     scrollTrigger: '.num3',
-     y:-50
+     y:-210
 });
 gsap.to('.num4',{
     opacity:1, 
     delay:0.5,
-    duration:1,
+    duration:0.5,
     scrollTrigger: '.num4',
-     y:-50
+     y:-210
 });
 gsap.to('.num5',{
     opacity:1, 
     delay:0.2,
-    duration:1,
+    duration:0.5,
     scrollTrigger: '.num5',
-     y:-50
+     y:-210
 });
 gsap.to('.num6',{
     opacity:1, 
     delay:0.5,
-    duration:1,
+    duration:0.5,
     scrollTrigger: '.num6',
-     y:-50
+     y:-210
+});
+gsap.to('.one--link',{
+    opacity:1, 
+    delay:0.5,
+    duration:1,
+     y:-30
+});
+gsap.to('.two--link',{
+    opacity:1, 
+    delay:0.7,
+    duration:1,
+     y:-30
+});
+gsap.to('.three--link',{
+    opacity:1, 
+    delay:0.9,
+    duration:1,
+     y:-30
+});
+
+
+gsap.to('.ds--1',{
+    opacity:1, 
+    delay:0.5,
+    duration:0.5,
+    scrollTrigger: '.ds--1',
+     y:-51
+});
+gsap.to('.ds--2',{
+    opacity:1, 
+    delay:0.5,
+    duration:0.5,
+    scrollTrigger: '.ds--2',
+     y:-51
+});
+gsap.to('.ds--3',{
+    opacity:1, 
+    delay:0.5,
+    duration:0.5,
+    scrollTrigger: '.ds--3',
+     y:-51
 });
